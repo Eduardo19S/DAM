29c616f373c15a43b464d59d09ff14eda36ebd1a
xp <xp@CETYSDAM00>
2019-11-29 15:58:03 +0100

    Con el cuarto ejercicio terminado.

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -29,8 +29,29 @@ public class EjerciciosBasicosJava {
         return 2;//El resto de casos , que son en los que voy a más de 80
     }
     
-  
-
+    public boolean muyVanidoso(int numero){
+        if (numero % 11 == 0 || numero % 11 == 1){
+            return true;
+        }
+        return false;
+        
+        // Otra posible solución es :
+        // return (numero % 11 == 0 || numero % 11 == 1)
+    }
+    
+    public boolean contesta( boolean matinal , boolean madre , boolean dormido){
+        if (madre){
+            return true;
+        }
+        if (matinal){
+            return true;
+        }
+        if (dormido){
+            return false;
+        }
+        return false;
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -44,6 +65,16 @@ public class EjerciciosBasicosJava {
         System.out.println(ejercicios.multa(60, false));
         System.out.println(ejercicios.multa(65, false));
         System.out.println(ejercicios.multa(65, true));
+        
+        // Testeo tercer ejercicio
+        System.out.println("muyVanidoso(22) " + ejercicios .muyVanidoso(22));
+        System.out.println("muyVanidoso(23) " + ejercicios .muyVanidoso(23));
+        System.out.println("muyVanidoso(24) " + ejercicios .muyVanidoso(24));
+        
+        //Testeo cuarto ejercicio
+         System.out.println(ejercicios.contesta(false , false , false));
+         System.out.println(ejercicios.contesta(false , false , true));
+         System.out.println(ejercicios.contesta(true , false , false));
     }
     
 }
